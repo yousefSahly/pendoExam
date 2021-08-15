6de844ce8b84dc9eaf8154c9b302b092cb7e74b8
yousefSahly <yousef.sahly92@gmail.com>
2021-08-15 17:54:59 +0300

    Initial Commit

---
+++ pendoExam/pendoExamApp.swift
@@ -0,0 +1,17 @@
+//
+//  pendoExamApp.swift
+//  pendoExam
+//
+//  Created by Yousef on 15/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct pendoExamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
